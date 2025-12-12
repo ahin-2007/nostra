9186ac04647201729bd66cf16e55b8d8a85c17b5
ahin-2007 <akhinkr14@gmail.com>
2025-12-12 15:45:50 +0530

    kytd

---
+++ script.js
@@ -0,0 +1,9 @@
+var menuopen=document.getElementById("menuopen");
+var menuclose=document.getElementById("menuclose");
+var menuitems=document.getElementById("menuitems")
+menuopen.addEventListener("click",function(){
+    menuitems.style.left="50%";
+})
+menuclose.addEventListener("click",function(){
+    menuitems.style.left="-50%"
+})
